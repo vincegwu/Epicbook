9b75ff4890fad4bec4a676ff440f450e27c7f890
vincegwu <egwuoko70@gmail.com>
2025-10-04 23:12:28 +0000

    Epicbook Deployment on Azure using Terraform

--- models/index.js
+++ models/index.js
@@ -3,27 +3,37 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const basename = path.basename(module.filename);
+
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+
 let db = {};
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+// SSL configuration for Azure MySQL
+const sslOptions = process.env.DB_SSL === "true" ? { ssl: { rejectUnauthorized: true } } : {};
+
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    dialectOptions: sslOptions,
+    logging: console.log
+  }
+);
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
-  })
-  .forEach((file) => {
+  .filter(file => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
+  .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -33,3 +43,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
